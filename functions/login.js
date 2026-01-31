00e1df1adfb2246be37abacf13152bf69d5a0c71
Muslim@1446 <m-11105557@moe-dl.edu.my>
2026-01-31 16:49:16 +0800

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -8,12 +8,9 @@ export async function onRequestPost(context) {
   // 2. Use SameSite=Lax to allow top-level navigation usage
   const cookieVal = `TUWA_PREMIUM=true; Path=/; ${isHttps ? 'Secure;' : ''} HttpOnly; SameSite=Lax; Max-Age=31536000`;
 
-  const headers = new Headers();
-  headers.append("Set-Cookie", cookieVal);
-  
   return new Response(JSON.stringify({ status: "activated" }), {
     headers: { 
-      ...headers, 
+      "Set-Cookie": cookieVal,
       "Content-Type": "application/json",
       "Cache-Control": "no-store, no-cache"
     }
